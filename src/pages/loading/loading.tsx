5d4f2a83b0ab155ec146be622a458612eefde5ce
Onoriode <onoriodeumukoro8@gmail.com>
2025-06-20 00:50:20 +0100

    created loading spinner

---
+++ src/pages/loading/loading.tsx
@@ -0,0 +1,11 @@
+
+import './loading.css'
+
+
+const loading = () => (
+    <div className='loading_wrapper'>
+        <span className="loading_loader"></span>
+    </div>
+)
+
+export default loading;
