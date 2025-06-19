2952a72014a6aae90fef6a7b98430270f8f8c295
Onoriode <onoriodeumukoro8@gmail.com>
2025-06-20 00:46:56 +0100

    added Routes for login, register and dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React, { useContext, Suspense } from 'react'
 import { Routes, Route } from 'react-router-dom'
 
+import Loading from './pages/loading/loading'
 import Layout from "./pages/layout/layout"
 import ContextProvider from './hooks/contextProvider'
 import AuthContext from './hooks/context'
 
-
+const Register = React.lazy(() => import("./components/register/register"))
 const Login = React.lazy(() => import("./components/login/login"))
 
 // interface AuthenticationProps {
@@ -19,12 +20,15 @@ function App() {
   return (
     <ContextProvider>
       <Routes>
-            {!authenticationDataProps && <Route path="/" element={<Suspense fallback={<p>Loading...</p>}><Login /></Suspense>} />}
+            {/* {!authenticationDataProps && <Route path="/" element={<Suspense fallback={<Loading />}><Home /></Suspense>} />} */}
+            {!authenticationDataProps && <Route path="/login" element={<Suspense fallback={<Loading />}><Login /></Suspense>} />}
+            {!authenticationDataProps && <Route path="/register" element={<Suspense fallback={<Loading />}><Register /></Suspense>} />}
+            {/* {!authenticationDataProps && <Route path="/user/reset_password/email" element={<Suspense fallback={<Loading />}><ResetPassword /></Suspense>} />} */}
       </Routes>
       {authenticationDataProps && <Layout>
         <Routes>
-            {/* {authenticationDataProps && <Route path="/" element={<Suspense fallback={<p>Loading...</p>}><Login /></Suspense>} />} */}
-            {/* <Route path="/user/register" element={<Suspense><Register /></Suspense>} /> */}
+            {/* {authenticationDataProps && <Route path="/" element={<Suspense fallback={<Loading />}><Login /></Suspense>} />} */}
+            {/* <Route path="/user/register" element={<Suspense fallback={<Loading />}><Register /></Suspense>} /> */}
         </Routes>
       </Layout>}
     </ContextProvider>
